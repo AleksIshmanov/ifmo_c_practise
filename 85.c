e2068de1950512212ad70db22930bebc9f186dce
cvyatoslavka@yandex.ru <cvyatoslavka@yandex.ru>
2019-11-06 13:45:58 +0300

    85.c done

--- 85.c
+++ 85.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
+void printfMatrix(int m, int n, int matrix[m][n]){
+	for(int i=0;i<m;i++){
+		for(int k=0;k<n;k++){
+			printf("%d \t", matrix[i][k]);
+		}
+		printf("\n");
+	}
+	
+}
+
 int main(){
 	int m, n;
 	scanf("%d%d", &m, &n);
-
 	int matrix[m][n];
 
-	int num=1;
-	int exIndex=0; //индекс крайнего столбца/строки
 	
-	int len= m>n ? m : n;
+	int num=1;
+	int len= m<n ? m : n;
+	len = (len/2)+(len%2); //кол-во проходов
+
 	for(int i=0; i<len;i++){
 		for(int k=i;k<n-i;k++) matrix[i][k]=num; // верхняя строка
 		for(int k=i;k<n-i;k++) matrix[m-i-1][k]=num; // нижняя строка
@@ -17,24 +27,13 @@ int main(){
 		for(int k=i;k<m-i;k++) matrix[k][i] = num; //левый столбец
 		for(int k=i;k<m-i;k++) matrix[k][n-i-1] = num; //правый столбец
 		
-		//puts("gg");
+		printf("\n_%d_\n", i);
+		printfMatrix(m,n, matrix);
 
-		/*
-			matrix[k][i] = num; //верхняя строка
-	 		matrix[n-k-1][i] = num;	//нижняя строка
-			matrix[i][k] = num; //левый столбец
-			matrix[m-k-1][i] = num; //правый столбе
-		*/
-
-		//exIndex++;
 		num++;
 	}
 
-	for(int i=0;i<m;i++){
-		for(int k=0;k<n;k++){
-			printf("%d \t", matrix[i][k]);
-		}
-		printf("\n");
-	}
+	puts("\n");
+	printfMatrix(m,n, matrix);
 
 }
