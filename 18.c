5a482ee1929881079aa1e584dc111f7339a6749b
cvyatoslavka@yandex.ru <cvyatoslavka@yandex.ru>
2019-11-08 04:47:06 +0300

    Попробовал переделать на рекурсию, не работает

--- 18.c
+++ 18.c
@@ -22,29 +22,49 @@ void mathAction(const char sign, char *left, char *right) {
 
 //проверяет является ли математическим знаком
 //в случае ошибки завершает приложение
-int isSign(const char sign){
+int isMathSign(const char sign){
     char signs[4] = {'+','-','*','/'};
     char nums[10] = {'0','1','2','3','4','5','6','7','8','9'};
 
+		printf("%c ", sign);
+
     for (int i=0;i<4;i++) {
         if(sign==signs[i]){
-            return 1;
+            return TRUE;
         }
     }
 
     for (int i=0;i<10;i++) {
         if(sign==nums[i]){
-            return 0;
+            return FALSE;
         }
     }
-
-    if(sign=='\0')
-        return 1;
 
     //попадем сюда в случае, если не мат. символ и не число
     printf("Incorrect character inside the equation: [%c] -> ASCII[%d]", sign, sign);
     exit(0);
 }
+
+
+/**
+ * Рекурсивная функция для определения числа слева от математического знака 
+ *
+ * @return int размер полученного числа
+ * @change char* leftNumCharArray - динамически изменяет его размер и содержимое 
+*/
+void getLeftNum(char* leftNumCharArray, char* str, int count){
+	if(isMathSign(str[count])){ //условие выхода - символ является математическим знаком
+		leftNumCharArray = malloc(count*sizeof(str[0])); //создаем число в формате char[Count-1] размером в количество найденных рекурсий
+		printf("\n%d %d ", count*sizeof(str[0]), count);
+		return;
+	}
+
+	//leftNumCharArray = (char*) realloc(leftNumCharArray, count*sizeof(str[0]));
+	getLeftNum(leftNumCharArray, str, count+1); //рекурсия
+	printf("\nggwp %s %d\n", leftNumCharArray, count);
+	leftNumCharArray[count] = str[count]; //присваиваем значения
+}
+
 
 int main(){
     char str[256], left[10], right[10];;
@@ -54,12 +74,17 @@ int main(){
     int lenLeft=0, lenRight=0; //длина строки, нужна в качества индекса для
 
     char sign;
-    //сохраняем первое число в буфер
-    while(isSign(str[i]) == 0 ){
-        left[lenLeft] = str[i];
-        lenLeft++;
-        i++;
-    }
+   
+	  char *leftNumCharArray;	
+		getLeftNum(leftNumCharArray, str, 0);//получаем первое число
+		
+		printf("done");
+
+  //  for(int i=0;i<len;i++) printf("%d ", leftNumCharArray[i]);
+		
+		
+
+		/*
     sign = str[i];
     i++;
 
@@ -73,11 +98,15 @@ int main(){
 
         mathAction(sign, left, right);
         sign = str[i];
-        i++;
+				
+				i++;
     }
 
 
     int result = atoi(left);
+		*/
 
-    printf("%d\n", result);
+    //printf("%d\n", result);
+		//
+		return 0;
 }
