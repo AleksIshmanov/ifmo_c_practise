35ae8871a32c606dbfa952ae80c39c39c6bacbc4
Aleks <aleks@MacBook-Pro-macos.local>
2020-06-05 19:27:18 +0300

    lab 6 updated

--- CPP/6_6.cpp
+++ CPP/6_6.cpp
@@ -1,149 +1,149 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <locale.h>
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <limits>
 
-#define FILENAME "mytest.txt"
-
+using namespace std;
 /*
-Сформировать массив на диске, содержащий сведения об ассортименте обуви в магазине фирмы. Класс содержит поля: артикул, наименование, количество, стоимость одной пары. Артикул начинается с буквы Д – для дамской обуви, М – для мужской, П – для детской. 
-Написать программу, которая выбирает необходимую информацию с диска и выводит на экран: 
-- сведения о наличии и стоимости обуви артикула X; 
-- ассортиментный список дамской обуви с указанием наименования и имеющегося в наличии числа пар каждой модели. 
-
+ Список результатов метеорологических наблюдений по месяцам. Элемент списка хранит следующие данные: период наблюдения (месяц и год), количество дней месяца, минимальная зафиксированная температура воздуха, максимальная зафиксированная температура воздуха. Определить год с наиболее высокой минимальной температурой, зафиксированной в течение зимних месяцев.
 */
 
-class FileController(){
-private:
-    FILE* file;
-
-public:
-    FileController(const string& filename){
-        if ((this->file = fopen(filename, "r")) == NULL){
-            printf("Couldn't open file");
-            exit(0);
-        }
-        return this->file;
-    }
-    
-    ~FileController(){
-        cout << "Close file" << endl;
-        fclose(this->file);
-    }
-    
-    read_line(Boot& products, int i){
-        return fscanf(
-            this->file, 
-            "%s%*c %i %s %f",
-            products[i].article,
-            products[i].count, 
-            products[i].name,  
-            products[i].price
-        );
-    }
-    
-    append_line(const string& line){
-        
-    }    
+int rand_int(int min, int max)
+{
+    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0); 
+    return static_cast<int>(rand() * fraction * (max - min + 1) + min);
 }
 
-class Catalog(){
-    
-private:
-    FileController file;
-    struct Boot products[256]; //множество со всем ассортиментом товаров
-    
-public:
-    static enum Articles {
-        ALL = 0,
-        KID = 'K',
-        MAN = 'M',
-        WOMAN = 'W'
-    };
-    
-    Catalog(const string& filename){
-        this->file(filename);
-        cout << file << endl;
+enum MonthDateNames {
+  Jan = 1,
+  Feb,
+  Mar,
+  Apr,
+  May,
+  Jun,
+  Jul,
+  Aug,
+  Sep,
+  Oct,
+  Nov,
+  Dec,
+};
+
+struct Date{
+    int month=rand_int(1,12);
+    int year=rand_int(2018,2020);
+    int day=rand_int(1,31);
+};
+
+struct Experiment {
+  Date date {};
+  int minAirTemperature;
+  int maxAirTemperature;
+
+  void set_rand(){
+    minAirTemperature = rand_int(-50, 0);
+    maxAirTemperature = rand_int(0,50);
+  };
+
+  friend std::ostream& operator<< (std::ostream &out, const Experiment& experiment);
+};
+
+/* singly linked list for struct Experiment */
+struct Node{
+    Node* next = nullptr;
+    Experiment experiment;
+
+    /* Given a reference (point to pointer)  
+      to the head of a list and an Experiment,  
+      inserts a new node on the front of the list. */
+    void add(Experiment experiment){
+
+      next = new Node;
+      next->experiment = experiment;
+
     }
-    
-    void create(){
-        bool isUserWantAddNew;
-        string finalString;
-        
 
-        char article;
-        char count[10];
-        char name[51];
-        char price[5];	
-        char space = ' ';
-
-        puts("------------------------------------");
-        printf("Хотите добавить новый товар? [1/0]: ");
-        scanf("%i", &isUserWantAddNew);
-
-        if(isUserWantAddNew){
-//            cout << "Введите строку в формате АРТИКЛЬ;КОЛИЧЕСТВО;НАИМЕНОВАНИЕ;ЦЕНА" << endl;
-//            cin >> article >> count >> name >> price;
-            
-            printf("Выберите один артикл товара [%c|%c|%c]: ", MAN, WOMAN, KID);
-            cin >> article;
-
-            printf("Какое количество товара находится в наличии? ");
-            cin >> count;
-
-            printf("Введите наименование товара (!ПРОБЕЛЫ ЗАПРЕЩЕНЫ): ");
-            cin >> name;
-
-            printf("Введите цену товара: ");
-            cin >> price;
-
-            // TODO make it by stringStream;
-            finalString = article+" "+count+" "+name+" "+price+"\n";
-            printf("final String: %s\n", finalString);
-            
-            //save a file and ask again
-            file.append_line(finalString);
-            this->create();
-        }
-        return 0;
+    /* Given a reference (pointer to pointer) 
+    to the head of a list and an Experiment, inserts 
+    a new node on the front of the list. */
+    void push(Node** head_ref, Experiment experiment)  
+    {  
+        /* 1. allocate node */
+        Node* new_node = new Node(); 
+      
+        /* 2. put in the data */
+        new_node->experiment = experiment;  
+      
+        /* 3. Make next of new node as head */
+        new_node->next = (*head_ref);  
+      
+        /* 4. move the head to point to the new node */
+        (*head_ref) = new_node; 
     }
-    
-    
-    void print(const enum Articles article){
-        for(int i=0; file.read_line(products, i)!= EOF; i++){
-            if(article==ALL){
-                print_line(i);
-                continue;
-            }
+};
 
-            if(article==products[i].article){
-                print_line(i);
-            }
-        }
-    }
-        
-    print_line(const int& i){
-        return fscanf(
-            this->file, 
-            "%s%*c %i %s %f",
-            this->products[i].article,
-            this->products[i].count, 
-            this->products[i].name,  
-            this->products[i].price
-        );
-    }
-    
-    
-    
+std::ostream& operator<< (std::ostream &out, const Experiment& experiment){
+  out << experiment.date.year << "-";
+  out << setw(2) << setfill('0') << experiment.date.month  << "-";
+  out << setw(2) << setfill('0') << experiment.date.day << " ";
+
+  out << " | " << setw(2) << setfill('0') << experiment.maxAirTemperature;
+  out << " | " << setw(3) << setfill(' ') << experiment.minAirTemperature;
+  out << " | ";
+
+  return out;
 }
 
+void set_experiments(Node** experiments, int& count){
+  if(count<1) {
+    return;
+  }
+
+  Experiment experiment;
+  experiment.set_rand();
+  (*experiments)->push(experiments, experiment);
+
+  cout << (*experiments)->experiment << *experiments << endl;
+  set_experiments(experiments, --count);
+}
+
+int find_warm_year(Node* experiments, int min=numeric_limits<int>::max()){
+
+  if(experiments==nullptr){
+    cout << "end" << endl;
+    return min;
+  }
+
+  Experiment experiment = experiments->experiment;
+  int month = experiment.date.month;
+  bool isWinterMonth = (month==Jan) || (month==Feb) || (month=Dec);
+  if(isWinterMonth) {
+    if(experiment.minAirTemperature<min) min=experiment.minAirTemperature;
+  }
+
+  find_warm_year(experiments->next, min);
+};
 
 int main(){
-    Catalog boots('boots.txt');
-    
-    boots.create();
-    boots.print(ALL);
-    boots.print(KID);
-    boots.print(MAN);
-    boots.print(WOMAN);
+    srand(unsigned(time(0)));
+
+    struct Node* experiments = nullptr;
+    Node* begin = experiments;
+
+    int count = 1000;
+
+    Experiment experiment;
+    set_experiments(&experiments, count);
+
+    while(experiments->next!=nullptr){
+      cout << experiments->experiment << endl;
+      experiments = experiments->next;
+    }
+
+    cout << endl;
+    experiments = begin;
+
+    // cout << endl << find_warm_year(experiments) << endl;
+
+    return 1;
 }
